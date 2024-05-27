6a92df0dfdcf0c795a9945f3bbd623171135931b
Will <williamorringe@gmail.com>
2024-05-27 23:01:44 +0100

    chore: Refactor Navbar component and update genre list

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -27,111 +27,117 @@ import { genres } from './genres'
 
 const Navbar = () => {
     return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/"
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faLaptop}
-                                            className="h-6 w-6"
-                                        />
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            shadcn/ui
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Beautifully designed components that
-                                            you can copy and paste into your
-                                            apps. Accessible. Customizable. Open
-                                            Source.
-                                        </p>
-                                    </a>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/docs" title="Introduction">
-                                Re-usable components built using Radix UI and
-                                Tailwind CSS.
-                            </ListItem>
-                            <ListItem
-                                href="/docs/installation"
-                                title="Installation"
-                            >
-                                How to install dependencies and structure your
-                                app.
-                            </ListItem>
-                            <ListItem
-                                href="/docs/primitives/typography"
-                                title="Typography"
-                            >
-                                Styles for headings, paragraphs, lists...etc
-                            </ListItem>
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Shows</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                            {shows.map((show) => (
-                                <ListItem
-                                    key={show.title}
-                                    title={show.title}
-                                    href={show.href}
-                                >
-                                    {show.description}
+        <div className="flex flex-row justify-between">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                <li className="row-span-3">
+                                    <NavigationMenuLink asChild>
+                                        <a
+                                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                            href="/"
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faLaptop}
+                                                className="h-6 w-6"
+                                            />
+                                            <div className="mb-2 mt-4 text-lg font-medium">
+                                                shadcn/ui
+                                            </div>
+                                            <p className="text-sm leading-tight text-muted-foreground">
+                                                Beautifully designed components
+                                                that you can copy and paste into
+                                                your apps. Accessible.
+                                                Customizable. Open Source.
+                                            </p>
+                                        </a>
+                                    </NavigationMenuLink>
+                                </li>
+                                <ListItem href="/docs" title="Introduction">
+                                    Re-usable components built using Radix UI
+                                    and Tailwind CSS.
                                 </ListItem>
-                            ))}
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Genres</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                            {genres.map((genre) => (
-                                <Fragment key={genre.title}>
+                                <ListItem
+                                    href="/docs/installation"
+                                    title="Installation"
+                                >
+                                    How to install dependencies and structure
+                                    your app.
+                                </ListItem>
+                                <ListItem
+                                    href="/docs/primitives/typography"
+                                    title="Typography"
+                                >
+                                    Styles for headings, paragraphs, lists...etc
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Shows</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                {shows.map((show) => (
                                     <ListItem
-                                        title={genre.title}
-                                        href={genre.href}
-                                        icon={genre.icon}
+                                        key={show.title}
+                                        title={show.title}
+                                        href={show.href}
                                     >
-                                        {genre.description}
+                                        {show.description}
                                     </ListItem>
-                                </Fragment>
-                            ))}
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Link href="/docs" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={navigationMenuTriggerStyle()}
-                        >
-                            Documentation
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <ThemeSwitcher />
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <Avatar>
-                        <AvatarImage
-                            src="https://github.com/shadcn.png"
-                            alt="@shadcn"
-                        />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Genres</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                {genres.map((genre) => (
+                                    <Fragment key={genre.title}>
+                                        <ListItem
+                                            title={genre.title}
+                                            href={genre.href}
+                                            icon={genre.icon}
+                                        >
+                                            {genre.description}
+                                        </ListItem>
+                                    </Fragment>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
+                                Documentation
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <ThemeSwitcher />
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Avatar>
+                            <AvatarImage
+                                src="https://github.com/shadcn.png"
+                                alt="@shadcn"
+                            />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     )
 }
 
