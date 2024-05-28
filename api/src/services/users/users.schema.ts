03a49a4fd442a16c1d334f3eb76eb1a63d770ffd
Will <williamorringe@gmail.com>
2024-05-28 20:49:21 +0100

    add stuff

--- api/src/services/users/users.schema.ts
+++ api/src/services/users/users.schema.ts
@@ -1,19 +1,18 @@
 // // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
 import { resolve } from '@feathersjs/schema'
-import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
+import { Type as z, getValidator, querySyntax } from '@feathersjs/typebox'
 import type { Static } from '@feathersjs/typebox'
-import { passwordHash } from '@feathersjs/authentication-local'
 
 import type { HookContext } from '../../declarations'
 import { dataValidator, queryValidator } from '../../validators'
 import type { UserService } from './users.class'
 
 // Main data model schema
-export const userSchema = Type.Object(
+export const userSchema = z.Object(
   {
-    id: Type.Number(),
-    email: Type.String(),
-    password: Type.Optional(Type.String())
+    id: z.Number(),
+    email: z.String(),
+    auth0Id: z.Optional(z.String())
   },
   { $id: 'User', additionalProperties: false }
 )
@@ -21,38 +20,31 @@ export type User = Static<typeof userSchema>
 export const userValidator = getValidator(userSchema, dataValidator)
 export const userResolver = resolve<User, HookContext<UserService>>({})
 
-export const userExternalResolver = resolve<User, HookContext<UserService>>({
-  // The password should never be visible externally
-  password: async () => undefined
-})
+export const userExternalResolver = resolve<User, HookContext<UserService>>({})
 
 // Schema for creating new entries
-export const userDataSchema = Type.Pick(userSchema, ['email', 'password'], {
+export const userDataSchema = z.Pick(userSchema, ['auth0Id'], {
   $id: 'UserData'
 })
 export type UserData = Static<typeof userDataSchema>
 export const userDataValidator = getValidator(userDataSchema, dataValidator)
-export const userDataResolver = resolve<User, HookContext<UserService>>({
-  password: passwordHash({ strategy: 'local' })
-})
+export const userDataResolver = resolve<User, HookContext<UserService>>({})
 
 // Schema for updating existing entries
-export const userPatchSchema = Type.Partial(userSchema, {
+export const userPatchSchema = z.Partial(userSchema, {
   $id: 'UserPatch'
 })
 export type UserPatch = Static<typeof userPatchSchema>
 export const userPatchValidator = getValidator(userPatchSchema, dataValidator)
-export const userPatchResolver = resolve<User, HookContext<UserService>>({
-  password: passwordHash({ strategy: 'local' })
-})
+export const userPatchResolver = resolve<User, HookContext<UserService>>({})
 
 // Schema for allowed query properties
-export const userQueryProperties = Type.Pick(userSchema, ['id', 'email'])
-export const userQuerySchema = Type.Intersect(
+export const userQueryProperties = z.Pick(userSchema, ['id', 'auth0Id'])
+export const userQuerySchema = z.Intersect(
   [
     querySyntax(userQueryProperties),
     // Add additional query properties here
-    Type.Object({}, { additionalProperties: false })
+    z.Object({}, { additionalProperties: false })
   ],
   { additionalProperties: false }
 )
