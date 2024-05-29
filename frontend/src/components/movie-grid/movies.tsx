81b2912e6082bca012bbfb1cdebf5c22039389eb
Will <williamorringe@gmail.com>
2024-05-29 19:10:45 +0100

    chore: Add lazy loading for images and update user profile display

--- frontend/src/components/movie-grid/movies.tsx
+++ frontend/src/components/movie-grid/movies.tsx
@@ -1,35 +1,55 @@
+'use client'
 import { Fragment } from 'react'
 import Image from 'next/image'
+import { useUser } from '@auth0/nextjs-auth0/client'
 export const MoviesGrid = ({
     movieList,
 }: {
     movieList: { image: string; title: string }[]
 }) => {
+    const { user, error, isLoading } = useUser()
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>{error.message}</div>
     return (
-        <div className="grid grid-cols-4 gap-x-4 gap-y-[10px]">
-            {movieList.map((movieInfo, i: number) => (
-                <Fragment key={i}>
-                    <div
-                        className="group relative hover:cursor-pointer col-span-1 h-[400px]
+        <>
+            {user && (
+                <div>
+                    <Image
+                        src={user?.picture ?? ''}
+                        alt={user?.name ?? 'FAKE'}
+                        width={300}
+                        height={300}
+                    />
+                    <h2>{user?.name}</h2>
+                    <p>{user?.email}</p>
+                </div>
+            )}
+            <div className="grid grid-cols-4 gap-x-4 gap-y-[10px]">
+                {movieList.map((movieInfo, i: number) => (
+                    <Fragment key={i}>
+                        <div
+                            className="group relative hover:cursor-pointer col-span-1 h-[400px]
          rounded-lg shadow-good transition-transform duration-300 ease-in-out transform hover:scale-110 hover:z-10 z-1"
-                    >
-                        <Image
-                            key={i}
-                            src={movieInfo.image}
-                            alt="movie poster"
-                            width={500}
-                            height={500}
-                            className="h-full w-full object-cover col-span-1 z-1 rounded-lg"
-                        />
-                        {/* <div className="bg-gray-100 w-full h-full"></div> */}
-                        <div className="absolute left-0 bottom-0 z-40 w-full h-20 bg-white dark:bg-slate-950 p-4 opacity-0 group-hover:opacity-80 rounded-b-lg -mt-1">
-                            <h2 className="text-lg font-semibold dark:text-white text-black">
-                                {movieInfo.title}
-                            </h2>
+                        >
+                            <Image
+                                key={i}
+                                src={movieInfo.image}
+                                alt="movie poster"
+                                width={500}
+                                height={500}
+                                className="h-full w-full object-cover col-span-1 z-1 rounded-lg"
+                            />
+                            {/* <div className="bg-gray-100 w-full h-full"></div> */}
+                            <div className="absolute left-0 bottom-0 z-40 w-full h-20 bg-white dark:bg-slate-950 p-4 opacity-0 group-hover:opacity-80 rounded-b-lg -mt-1">
+                                <h2 className="text-lg font-semibold dark:text-white text-black">
+                                    {movieInfo.title}
+                                </h2>
+                            </div>
                         </div>
-                    </div>
-                </Fragment>
-            ))}
-        </div>
+                    </Fragment>
+                ))}
+            </div>
+        </>
     )
 }
