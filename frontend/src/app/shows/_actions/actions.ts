fb00ac02ffda71873c7293d3c604242b1b1db1b0
Will <williamorringe@gmail.com>
2024-05-30 22:14:06 +0100

    chore: Add lazy loading for images and update user profile display

--- frontend/src/app/shows/_actions/actions.ts
+++ frontend/src/app/shows/_actions/actions.ts
@@ -1,11 +1,13 @@
+'use server'
 export default async function getShows(genres: string) {
     const apiKey = process.env.MOVIES_KEY
     const url = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`
     const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
     if (!response.ok) {
         return { results: [] }
     }
-    const data = await response.json()
 
     return data
 }
